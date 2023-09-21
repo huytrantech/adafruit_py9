22e82700955dd2713ae0bc7819f5b61f7183e1ec
huytrantech <133479098+huytrantech@users.noreply.github.com>
2023-09-21 19:51:51 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -14,13 +14,13 @@ scheduler.SCH_Init()
 task2 = Task2()
 task3 = Task3()
 
-taskPhysical = physical.PhysicalTask()
+# taskPhysical = physical.PhysicalTask()
 
 
 scheduler.SCH_Add_Task(task2.Task2_Run, 1000,5000)
 scheduler.SCH_Add_Task(task3.Task3_Run, 3000,5000)
-scheduler.SCH_Add_Task(taskPhysical.soil_moisture,1000,5000)
-scheduler.SCH_Add_Task(taskPhysical.soil_temperature,1000,5000)
+# scheduler.SCH_Add_Task(taskPhysical.soil_moisture,1000,5000)
+# scheduler.SCH_Add_Task(taskPhysical.soil_temperature,1000,5000)
 while True:
     scheduler.SCH_Update()
     scheduler.SCH_Dispatch_Tasks()
